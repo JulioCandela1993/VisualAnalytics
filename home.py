4131cdff39d26a54bf25f4cdbc1854dd28ffeaeb
JulioCandela1993 <julio.christians.candela@est.fib.upc.edu>
2021-01-09 16:50:32 +0100

    Home

--- home.py
+++ home.py
@@ -4,6 +4,11 @@ from PIL import Image
 
 def app():
     st.title("Tobacco: a silent killer")
+    
+    st.markdown('''
+    Smoking is considered one of the biggest public health threats by the World Health Organization (WHO)
+    ''')
+    
     image = Image.open('img/smoking.jpg')
 
     st.image(image, caption='',
